53b4d757000fb8b0fb444e26426b2e5adcef9b26
Agustín López Zapata <aguslopezzapata10@gmail.com>
2024-10-30 10:25:45 -0500

    main2 ipynb added

--- utilities.py
+++ utilities.py
@@ -12,7 +12,7 @@ import numpy as np
 from PIL import Image, ImageOps
 from matplotlib import pyplot as plt
 import math
-from scipy.optimize import minimize
+from scipy.optimize import minimize, minimize_scalar
 
 
 # Function to read an image file from the disk
@@ -93,6 +93,69 @@ def ift(field):
 
     return ift
 
+def vortexConvolution(inp, l_vortex, plot = False):
+
+    width, height = (np.array(inp)).shape
+
+    # Size of the image
+    size = width 
+
+    # Generate a grid of x and y coordinates
+    y, x = np.ogrid[-size//2:size//2, -size//2:size//2]
+
+    # Compute the angle from the origin in radians, and wrap it to the range [0, 2π]
+    angle = np.arctan2(y, x)  # Angle in radians from -π to π
+    vortex = np.exp(1j * l_vortex * angle)
+    
+    # Plot the vortex
+    
+    if plot:
+        plt.imshow(np.angle(vortex), cmap='gray', extent=(-size//2, size//2, -size//2, size//2))
+        plt.colorbar(label="Angle (radians)")
+        plt.title("Angle from Origin (0 to 2π)")
+        plt.show()
+    
+    conv = (inp * vortex)
+    ift_conv = np.fft.ifftshift(np.fft.ifft2(np.fft.fftshift(conv)))
+
+    if plot: 
+        plt.imshow(intensity(ift_conv, True), cmap='grey', extent=(-size//2, size//2, -size//2, size//2))
+        plt.title("Hologram and vortex convolution in spectra")
+
+    return ift_conv
+
+
+# Spatial filtering process for FCF implementation
+def spatialFilteringCF_vortex(inp, width, height, l_vortex):
+    # inputs:
+    # inp - field
+    # width -
+    # height -
+    field_spec_vortex = vortexConvolution(inp, l_vortex)
+
+    # Finding the max peaks for +1 order in I or II quadrant
+    mask = np.zeros((width, height))
+    mask[0:height, 0:round(int(width/2))] = 1
+    field_spec_tem = field_spec_vortex * mask
+    maximum = np.amax(field_spec_tem)
+    fy_max, fx_max = np.where(field_spec_tem == maximum)
+
+    # Determination of the ROI size. To do this, we use the theoretical size of the +1 or -1 diffraction orders according...
+    # ... to the distance of their centers to the DC coordinates (middle point in the DHM hologram spectrum).
+    d = np.sqrt(np.power(fy_max - width / 2, 2) + np.power(fx_max - height / 2, 2))
+    radius = d / 3
+    mask = circularMask(width, height, radius, fy_max, fx_max, False)
+
+    # Filtering the hologram
+    tmp = field_spec_vortex * mask
+
+    # Coming back to spatial domain (retrieving filtered hologram)
+    out = np.fft.ifftshift(tmp)
+    out = np.fft.ifft2(out)
+
+    return out, fx_max, fy_max
+
+
 
 # Spatial filtering process for FCF implementation
 def spatialFilteringCF(inp, width, height):
@@ -172,6 +235,7 @@ def costFunction(seeds, width, height, holo_filter, wavelength, dxy, X, Y, fx_0,
     return J
 
 
+
 # function to retrieve the complex object information from an off-axis hologram
 def reconstruction(field, wavelength, dxy, distance):
     # inputs:
@@ -202,7 +266,7 @@ def reconstruction(field, wavelength, dxy, distance):
     print("Minimization process started.....")
     res = minimize(costFunction, seeds, args=(width, height, holo_filter, wavelength, dxy, X, Y, fx_0, fy_0, k),
                    method='Nelder-Mead', bounds=bounds, tol=1e-9)
-    print("Minimization process finished.")
+    print("Minimization process finished. Cost function value = {res.fun}")
     best_fx_max = res.x[0]
     best_fy_max = res.x[1]
     print('fx: ', best_fx_max)
@@ -220,7 +284,57 @@ def reconstruction(field, wavelength, dxy, distance):
     # propagation of the complex object filed by implemented the AngularSpectrum
     comp_phase = angularSpectrum(comp_phase, width, height, wavelength, distance, dxy)
 
-    return comp_phase
+    return comp_phase, res
+
+# function to retrieve the complex object information from an off-axis hologram
+def reconstruction_vortex(field, wavelength, dxy, distance, l_vortex):
+    # inputs:
+    # field: hologram to reconstruct
+    # wavelength: wavelength used to register the hologram
+    # dxy: pixel pitch of camera used to register the hologram
+    # distance: propagation distance
+    field = np.array(field)
+    width, height = field.shape
+
+    # Creating a mesh_grid to operate in world coordinates
+    Y, X = np.meshgrid(np.arange(-height / 2, height / 2), np.arange(-width / 2, width / 2), indexing='ij')
+    fx_0 = width / 2
+    fy_0 = height / 2
+    k = (2 * math.pi) / wavelength
+
+    # The spatial filtering process is executed
+    print("Spatial filtering process started.....")
+    holo_filter, fx_max, fy_max = spatialFilteringCF_vortex(field, width, height, l_vortex)
+    print("Spatial filtering process finished.")
+
+    # loading seeds
+    seeds = (fx_max[0], fy_max[0])
+    step = 1
+    bounds = [(fx_max - step, fx_max + step), (fy_max - step, fy_max + step)]
+
+    # minimization
+    print("Minimization process started.....")
+    res = minimize(costFunction, seeds, args=(width, height, holo_filter, wavelength, dxy, X, Y, fx_0, fy_0, k),
+                   method='Nelder-Mead', bounds=bounds, tol=1e-9)
+    print(f"Minimization process finished. Cost function value = {res.fun}")
+    best_fx_max = res.x[0]
+    best_fy_max = res.x[1]
+    print('fx: ', best_fx_max)
+    print('fy: ', best_fy_max)
+
+
+    # Best phase compensation
+    print("Phase compensation started....")
+    theta_x = math.asin((fx_0 - best_fx_max) * wavelength / (width * dxy))
+    theta_y = math.asin((fy_0 - best_fy_max) * wavelength / (height * dxy))
+    ref_wave = np.exp(1j * k * ((math.sin(theta_x) * X * dxy) + (math.sin(theta_y) * Y * dxy)))
+    comp_phase = holo_filter * ref_wave
+
+
+    # propagation of the complex object filed by implemented the AngularSpectrum
+    comp_phase = angularSpectrum(comp_phase, width, height, wavelength, distance, dxy)
+
+    return comp_phase, res
 
 
 # Function to propagate an optical field using the Angular Spectrum approach
@@ -250,3 +364,5 @@ def angularSpectrum(field, width, height, wavelength, distance, dxy):
     out = np.fft.ifftshift(out)
 
     return out
+
+
